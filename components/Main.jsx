8dc2d96c21216b9007a6573515021adf50ab3b33
tiff3b <tiffany3berger@gmail.com>
2026-02-14 11:03:15 -0500

    Updated section

--- components/Main.jsx
+++ components/Main.jsx
@@ -25,9 +25,20 @@ export default function Main (){
                 />
                 <button>Add ingredient</button>
             </form>
-            <ul>
-                {ingredientsListItems}
-            </ul>
+        {ingredients.length > 0 &&
+            <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                {ingredients.length > 3 && 
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>}
+            </section>
+        }
         </main>
     )
 }
